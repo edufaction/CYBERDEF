c845e4625fff4d8dc583eab90cbb51a215d18348
edufaction <eric.dupuis@cnam.fr>
2020-05-29 13:18:35 +0200

    2020-05-29-03

---
+++ Ressources/freemind.src/CYBERDEF101.mm
@@ -0,0 +1,42 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1590745829550" ID="ID_1238991130" MODIFIED="1590750702015">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      El&#233;ments de SECOPS<br />en Cyberd&#233;fense d'entreprise
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1590745922347" HGAP="-85" ID="ID_1070154731" MODIFIED="1590746846074" POSITION="right" TEXT="Risk Management" VSHIFT="-121"/>
+<node CREATED="1590746370326" HGAP="59" ID="ID_1905015109" MODIFIED="1590750558124" POSITION="left" TEXT="Information Security (InfoSec)" VSHIFT="-10">
+<node CREATED="1590750589078" ID="ID_717849387" MODIFIED="1590750939039" TEXT="S&#xe9;curit&#xe9; des Syst&#xe8;mes d&apos;informations">
+<node CREATED="1590750577636" ID="ID_344159654" MODIFIED="1590750587923" TEXT="Architectures de s&#xe9;curit&#xe9;"/>
+<node CREATED="1590750617905" ID="ID_1775338035" MODIFIED="1590750631777" TEXT="S&#xe9;curit&#xe9; p&#xe9;rimetriques"/>
+<node CREATED="1590750632876" ID="ID_870855073" MODIFIED="1590750640090" TEXT="S&#xe9;curit&#xe9; des terminaux"/>
+<node CREATED="1590750640961" ID="ID_1165243455" MODIFIED="1590750660361" TEXT="S&#xe9;curit&#xe9; Op&#xe9;rationelle"/>
+</node>
+<node CREATED="1590750762312" ID="ID_738739705" MODIFIED="1590751013671" TEXT="S&#xe9;curit&#xe9; dans les produits et services">
+<node CREATED="1590750864265" ID="ID_1561902219" MODIFIED="1590750935548" TEXT="Cycle de d&#xe9;veloppement de confiance (SecByDesign)">
+<node CREATED="1590751075791" ID="ID_66646535" MODIFIED="1590751088732" TEXT="Analyse de risque"/>
+<node CREATED="1590750801172" ID="ID_1296495866" MODIFIED="1590750817743" TEXT="Evaluation"/>
+<node CREATED="1590750818495" ID="ID_263719776" MODIFIED="1590750821292" TEXT="Audit"/>
+<node CREATED="1590750828590" ID="ID_1477065857" MODIFIED="1590750862042" TEXT="Conception"/>
+</node>
+<node CREATED="1590751039901" ID="ID_762986559" MODIFIED="1590751043361" TEXT="D&#xe9;veloppement">
+<node CREATED="1590750831529" ID="ID_1415477547" MODIFIED="1590750856245" TEXT="Programmation d&#xe9;fensive"/>
+<node CREATED="1590751060196" ID="ID_1660755090" MODIFIED="1590751063722" TEXT="Audit de code"/>
+</node>
+</node>
+</node>
+<node CREATED="1590746401217" HGAP="-73" ID="ID_1236301290" MODIFIED="1590746849639" POSITION="right" TEXT="VTI (Vulns/Threats/Incidents)" VSHIFT="159"/>
+<node CREATED="1590750702790" ID="ID_1884539217" MODIFIED="1590750728356" POSITION="left" TEXT="Gouvernance Risque et Conformit&#xe9;">
+<node CREATED="1590750675693" ID="ID_1992694621" MODIFIED="1590750680569" TEXT="Conformit&#xe9;"/>
+<node CREATED="1590750562029" ID="ID_1783601211" MODIFIED="1590750577080" TEXT="Politique de s&#xe9;curit&#xe9;"/>
+</node>
+</node>
+</map>
